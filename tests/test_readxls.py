83a368e872f8439c65a3160719501edba0505bcb
Louis <scu.louis@gmail.com>
2020-03-12 13:49:19 +0800

    fix xls name become english name

--- tests/test_readxls.py
+++ tests/test_readxls.py
@@ -3,6 +3,6 @@ from Library.ReadXls import ReadXls
 
 class TestXls:
     def test_sheetNameGreateThanZero(self):
-        xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
+        xlsObj = ReadXls(filename = 'templates/tableDefine_v1.0.xlsx')
         print(len(xlsObj.getSheetNames())) 
         assert len(xlsObj.getSheetNames()) > 0
