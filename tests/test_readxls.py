85e9208d9c087609d7bd710c33c6592e9049c932
Louis <scu.louis@gmail.com>
2020-03-13 14:59:57 +0800

    tableStructure.xlsx

--- tests/test_readxls.py
+++ tests/test_readxls.py
@@ -3,7 +3,7 @@ import pytest
 from Library.ReadXls import ReadXls 
 
 class TestXls:
-    xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
+    xlsObj = ReadXls(filename = 'templates/tableStructure.xlsx')
     
     def test_sheetNameGreateThanZero(self):
         print(f"\nsheet數： {len(self.xlsObj.getSheetNames())}") 
@@ -14,7 +14,6 @@ class TestXls:
 
     @pytest.mark.skip(reason="skip this")
     def test_getSheetData(self):
-        # xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
         for name in self.xlsObj.getSheetNames():
             print(f"sheet name:{name}")
             for row in self.xlsObj.getSheetData(name):
@@ -22,11 +21,10 @@ class TestXls:
 
     @pytest.mark.skip(reason="skip this")
     def test_getFieldTiyle(self):
-        # xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
         print(self.xlsObj.fieldTitle('FormMaster',3))
 
     def test_getRows(self):
-        # xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
-        for name in self.xlsObj.getSheetNames():
+        for name in self.xlsObj.getSheetNames(['Index']):
             print(f"sheet name:{name}")
-            print(self.xlsObj.GetRows('FormMaster',self.xlsObj.fieldTitle(name,3)))
+            for field in self.xlsObj.GetRows('FormMaster',self.xlsObj.fieldTitle(name,3)):
+                print(field)
