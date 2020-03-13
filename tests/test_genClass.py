c5959bcb78ac811740a0e48e23c76b2071e41fec
Louis <scu.louis@gmail.com>
2020-03-13 17:22:53 +0800

    app module path

--- tests/test_genClass.py
+++ tests/test_genClass.py
@@ -1,11 +1,13 @@
 import pytest
 from Library.ReadXls import ReadXls 
 from Library.GenCshapeClass import GenCshapeClass
+import sys
 
 class TestGen:
     xlsObj = ReadXls(filename = 'templates/tableStructure.xlsx')    
 
     def test_genClass(self):
+        sys.path.append('Library/GenCshapeClass.py')
         genClass = GenCshapeClass('templates','FormMaster')
         fieldTitles = self.xlsObj.fieldTitle('FormMaster',3)
         rows = self.xlsObj.GetRows('FormMaster',fieldTitles)
