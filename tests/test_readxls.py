b6a1aae39705baecd13a6f9dd2219f702cd907ed
Louis <scu.louis@gmail.com>
2020-03-13 12:13:31 +0800

    readxls base test

--- tests/test_readxls.py
+++ tests/test_readxls.py
@@ -4,10 +4,28 @@ from Library.ReadXls import ReadXls
 
 class TestXls:
     def test_sheetNameGreateThanZero(self):
-        xlsObj = ReadXls(filename = 'templates/tableDefine_v1.0.xlsx')
+        xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
         print(f"\nsheet數： {len(xlsObj.getSheetNames())}") 
         for name in xlsObj.getSheetNames():
-            print(f"\n {name}")
-            
+            print(f"{name}")
+
         assert len(xlsObj.getSheetNames()) > 0
 
+    @pytest.mark.skip(reason="skip this")
+    def test_getSheetData(self):
+        xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
+        for name in xlsObj.getSheetNames():
+            print(f"sheet name:{name}")
+            for row in xlsObj.getSheetData(name):
+                print(row)
+
+    @pytest.mark.skip(reason="skip this")
+    def test_getFieldTiyle(self):
+        xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
+        print(xlsObj.fieldTitle('FormMaster',3))
+
+    def test_getRows(self):
+        xlsObj = ReadXls(filename = 'templates/費用模組_v1.0.xlsx')
+        for name in xlsObj.getSheetNames():
+            print(f"sheet name:{name}")
+            print(xlsObj.GetRows('FormMaster',xlsObj.fieldTitle(name,3)))
